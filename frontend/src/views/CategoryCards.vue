3a55f03a949929fd2b9bd84aaea2865c675e02bf
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-05 20:05:28 +0300

    Update CategoryCards.vue

--- frontend/src/views/CategoryCards.vue
+++ frontend/src/views/CategoryCards.vue
@@ -4,12 +4,12 @@
     <div class="background-container"></div>
     
     <div class="page-header">
-      <button @click="$router.back()" class="back-button">
+      <!-- <button @click="$router.back()" class="back-button">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M19 12H5M12 19l-7-7 7-7"/>
         </svg>
         Back to Categories
-      </button>
+      </button> -->
       
       <!-- Centered category title and info -->
       <div class="category-header-content">
