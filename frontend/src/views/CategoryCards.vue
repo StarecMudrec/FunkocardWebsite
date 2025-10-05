4e8e4cf941d797c136b52c600d05dc4bc9fc8da8
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-05 20:25:43 +0300

    Update CategoryCards.vue

--- frontend/src/views/CategoryCards.vue
+++ frontend/src/views/CategoryCards.vue
@@ -285,8 +285,7 @@ export default {
 
 .search-input:focus {
   outline: none;
-  border-color: #434343;
-  border-width: 2px;
+  border-color: #555555;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
 }
 
