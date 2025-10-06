467fc71d945035a4c9840105a8def1e56badd6f3
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-06 17:10:34 +0300

    Update CategoryCards.vue

--- frontend/src/views/CategoryCards.vue
+++ frontend/src/views/CategoryCards.vue
@@ -14,7 +14,7 @@
         <!-- Search Bar -->
         <div class="search-container">
           <div class="search-input-wrapper">
-            <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <circle cx="11" cy="11" r="8"></circle>
               <path d="m21 21-4.3-4.3"></path>
             </svg>
