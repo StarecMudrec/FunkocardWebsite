055dbb5e3ad1e20396595842edcf92b8369efe08
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-12 17:01:54 +0300

    Update CardDetail.vue

--- frontend/src/views/CardDetail.vue
+++ frontend/src/views/CardDetail.vue
@@ -68,7 +68,7 @@
                 <div class="card-info-columns">
                   <div class="card-info-column">
                     <h3>
-                      Rarity:
+                      Rarity: {{ card.category }}
                       <span v-if="isUserAllowed" class="edit-icon" @click.stop="toggleEdit('category')">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                           <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
@@ -76,7 +76,7 @@
                         </svg>
                       </span>
                     </h3>
-                    <div class="category-container">
+                    <!-- <div class="category-container">
                       <p v-if="!editing.category">{{ card.category }}</p>
                       <input
                         v-else
@@ -88,7 +88,7 @@
                         class="edit-input"
                         maxlength="20"
                       >
-                    </div>
+                    </div> -->
                     <div v-if="categoryError" class="error-message">{{ categoryError }}</div>
                   </div>
                   <!-- Add Points column if needed -->
