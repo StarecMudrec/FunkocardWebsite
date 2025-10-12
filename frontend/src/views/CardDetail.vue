11e2bef5d09addd2de198cdf09206c1a3fc407ce
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-12 14:11:13 +0300

    Update CardDetail.vue

--- frontend/src/views/CardDetail.vue
+++ frontend/src/views/CardDetail.vue
@@ -981,7 +981,7 @@
     top: 0;
     left: 0;
     width: 100%;
-    height: 400px;
+    height: 100%;
     background-image: url('/background.jpg');
     background-size: cover;
     background-position: center 57%;
