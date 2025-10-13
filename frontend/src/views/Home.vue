6383dc87366f9838a86d2cc6ffc6e50ece628473
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-13 19:13:01 +0300

    sdfadv

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -135,7 +135,7 @@
 
 .category-card {
   border-radius: 12px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 2px 7px 10px 0px rgba(0, 0, 0, 0.4);
   cursor: pointer;
   transition: all 0.3s ease;
   overflow: hidden;
