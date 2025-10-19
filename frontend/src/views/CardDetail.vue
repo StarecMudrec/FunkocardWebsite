d75fbfef6b29d7a69927145a92dee98ac7831c02
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-10-19 14:01:55 +0300

    Update CardDetail.vue

--- frontend/src/views/CardDetail.vue
+++ frontend/src/views/CardDetail.vue
@@ -1010,42 +1010,42 @@
   }
 
   .slide-left-enter-from {
-    transform: translateX(100%) translateY(-100%); /* Added translateY */
+    transform: translateX(100%); /* Added translateY */
     opacity: 0;
   }
 
   .slide-left-enter-to {
-    transform: translateX(0) translateY(-100%); /* Added translateY */
+    transform: translateX(0); /* Added translateY */
     opacity: 1;
   }
 
   .slide-left-leave-from {
-    transform: translateX(0) translateY(0);
+    transform: translateX(0);
     opacity: 1;
   }
 
   .slide-left-leave-to {
-    transform: translateX(-100%) translateY(0);
+    transform: translateX(-100%);
     opacity: 0;
   }
 
   .slide-right-enter-from {
-    transform: translateX(-100%) translateY(-100%); /* Added translateY */
+    transform: translateX(-100%); /* Added translateY */
     opacity: 0;
   }
 
   .slide-right-enter-to {
-    transform: translateX(0) translateY(-100%); /* Added translateY */
+    transform: translateX(0); /* Added translateY */
     opacity: 1;
   }
 
   .slide-right-leave-from {
-    transform: translateX(0) translateY(0);
+    transform: translateX(0);
     opacity: 1;
   }
 
   .slide-right-leave-to {
-    transform: translateX(100%) translateY(0);
+    transform: translateX(100%);
     opacity: 0;
   }
 
