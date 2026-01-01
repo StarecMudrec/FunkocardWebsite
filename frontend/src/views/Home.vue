3656b0b173290f881bff4adefc6b9b3ef8a59b38
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2026-01-01 20:41:59 +0300

    Revert "Update Home.vue"
    This reverts commit 3e2e2fe90f6c67b4e317550fab52901fdc8a17a6.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -870,7 +870,7 @@ export default {
 
     shouldShowVideo(category) {
       const card = this.getNewestCardForCategory(category);
-      return card?.rarity === 'Limited \u26a0\ufe0f'; 
+      return card?.rarity === 'Limited ⚠️'; 
       console.log("AIJKFBYUIPEHJN")
     },
 
